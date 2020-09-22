e595f18cd5760400181520ef1f5316ac72127dd8
DerrickDarku <derrickalberto@ymail.com>
2020-09-22 15:18:51 +0000

    PROFILE MOBILE

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import {
+   Text, View, 
+   StyleSheet, Image } from 'react-native';
+
+
+
+
+function App(){
+  return <View style={styles.container}>
+     <Image source={require('./assets/profile.jpg')}  style={styles.image}/>
+    
+    <View style={styles.detailContainer}>
+    <Text style={styles.label}>Name</Text>
+    <Text style={[styles.label,styles.info]}>Derrick Alberto Darku</Text>
+  </View>
+
+  <View style={styles.detailContainer}>
+    <Text style={styles.label}>Email</Text>
+    <Text style={[styles.label,styles.info]}>Derrickalberto@ymail.com</Text>
+  </View>
+
+  <View style={styles.detailContainer}>
+    <Text style={styles.label}>Gender</Text>
+    <Text style={[styles.label,styles.info]}>Male</Text>
+  </View>
+
+  </View>
+ 
+  }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginHorizontal:5,
+    backgroundColor: "orange"
+  },
+  image:{
+    marginTop:100,
+    width:150,
+    height:150,
+    marginBottom:30,
+    alignSelf:"center",
+    borderRadius:75
+  },
+detailContainer:{
+  flexDirection:"row",
+  marginVertical:3
+},
+label:{
+  borderColor:"black",
+  borderWidth:1,
+  fontSize:20,
+  flex:2.5 ,
+  paddingHorizontal:5
+},
+info:{
+  flex:7.5
+}
+
+
+});
+
+
+
+export default App;
