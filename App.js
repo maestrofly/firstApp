5d54f2c467c52535e316d6cdcb7a5721c0b27347
DerrickDarku <derrickalberto@ymail.com>
2020-09-22 17:27:54 +0000

    first commit

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     marginHorizontal:5,
-    backgroundColor: "orange"
+    backgroundColor: "brown"
   },
   image:{
     marginTop:100,
